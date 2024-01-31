0213710a1eab5aef46f56cdcb883eb8c09693f31
laimax17 <caicai0660@gmail.com>
2024-01-30 23:42:13 -0500

    update

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -10,22 +10,40 @@ function createTRNode(colNodes) {
   return trNode;
 }
 
-function createTDNode(childNode) {
+function createTDNode(childNode) {    //child node
   let tdNode = document.createElement("td");
   tdNode.appendChild(childNode);
   return tdNode;
 }
 
-function createTxtNode(txt) {
+function createTxtNode(txt) {     // create the text
   let txtNode = document.createTextNode(txt);
   return txtNode;
+}
+
+function createButton(btnText, e, l) {     // create the button
+  let btnNode = document.createElement("button");
+  let btnNodeText = document.createTextNode(btnText);
+  btnNode.appendChild(btnNodeText);
+  btnNode.addEventListener(e,l)
+  return btnNode;
 }
 
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(createButton("Edit Text", "click", function() {
+      edit(col1);
+    }));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
+}
+
+function edit(node){
+  let newNode = document.createElement("input");
+  newNode.type = 'text';
+  newNode.placeholder = 'Enter Cell (x,y) ...';
+  node.replaceChild(newNode, node.childNodes[0]);
 }
